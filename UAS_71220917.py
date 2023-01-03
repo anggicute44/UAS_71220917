07bd799dbca901b01d6f43b39bf3690724ccd84d
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2023-01-03 14:49:26 +0700

    Add files via upload

---
+++ UAS_71220917.py
@@ -0,0 +1,84 @@
+print("==== selamat datang di XXV===")
+print("1.horror")
+print("2.action")
+   
+pilihan = int(input("silahkan mau nonton film genre apa:"))
+if pilihan =
+    print()
+elif pilihan =
+     print()
+else:
+   print("PILIHAN TIDAK TERSEDIA")      
+
+print("=== berikut pilihan film action yang seadng tayang===")
+print("1.the devil's light")
+print("2.pengabdi setan")
+
+pilihan= int(input("silahkan pilih mau nonton film apa :"))
+if pilihan =
+    print()
+elif pilihan =
+     print()
+else:
+   print("PILIHAN TIDAK TERSEDIA")      
+
+pilihan= int(input("mau pesan tiket sebanyak:"))
+
+print("total yang harus dibayar adalah rp. 73500")
+
+print("=== selamat datang di XXV====")
+pilihan=int(input("masukkan tanggal hari ini:"))
+
+print("1.horror")
+print("2.action")
+
+print("====berikut pilihan genre film yang tersedia===")
+print("1.black panther:wakanda forever")
+print("2.avatar: the may of water")
+
+pilihan= int(input("silahkan pilih mau nonton film apa :"))
+if pilihan =
+    print()
+elif pilihan =
+     print()
+else:
+   print("PILIHAN TIDAK TERSEDIA")      
+
+pilihan= int(input("mau pesan tiket sebanyak:"))
+
+print("total yang harus dibayar adalah rp.2375000")
+
+print("==== selamat datang di XXV===")
+pilihan=int(input("masukkan tanggal hari ini:"))
+
+print("====berikut pilihan genre film yang tersedia===")
+print("1.horror")
+print("2.action")
+
+pilihan= int(input("silahkan pilih mau nonton film apa :"))
+if pilihan =
+    print()
+elif pilihan =
+     print()
+else:
+   print("PILIHAN TIDAK TERSEDIA")      
+s
+pilihan= int(input("mau pesan tiket sebanyak:"))
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+             
+            
+
+
